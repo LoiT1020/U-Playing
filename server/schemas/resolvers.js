917ce86106b0d006cb9f5e22d342da25385ccd16
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-09-29 19:00:08 -0500

    updated typeDefs/Resolverss

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,18 +2,38 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, playlist } = require('../models');
 const { signToken } = require('../utils/auth');
 const {FetchGames, FetchSearch} = require('../utils/rawgAPI')
-const dotenv = require('dotenv')
 const fetch = require('node-fetch');
 const resolvers = {
+
     Query: {
         allGames : async() => {
             const response = await fetch(`https://rawg.io/api/games?token&key=${process.env.REACT_APP_API_KEY}`);
             return response.json();
         },
-        searchGame: async(_, {id}) => {
-            const response = await fetch(`https://api.rawg.io/api/games/${id}?token&key=${process.env.API_KEY}`);
+        searchGame: async(_, {id}) => { 
+            const response = await fetch(`https://api.rawg.io/api/games/${id}?token&key=${process.env.REACT_APP_API_KEY}`);
             return response.json();
+        },
+        user: async (parent, args, context) => {
+            if(context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: '',
+                    populate: ''
+                });
+                return user;
+            }
+            throw new AuthenticationError('Not Logged In');
+        },
+        playlist: async (parent, {_id}, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: '',
+                    populate: ''
+                });
+                return user.playlist.id(_id);
+            }
         }
+        
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,20 +4,15 @@ const typeDefs = gql`
 
   type GameSearched {
     id: ID
+    slug: String
     name: String
     description: String
     released: String
     background_image: String
     website: String
-    rating: Int
-    genres: [Genres]
-
+    platforms: [Platform]
   }
-  type Search {
-    count: Int!
-    results: [GameSearched]!
-
-  }
+  
   type Genres {
     id: Int
     name: String
@@ -28,7 +23,7 @@ const typeDefs = gql`
   }
   type Platform {
     platform: PlatformInfo
-    releasedAt: String
+    released_at: String
   }
   type Results {
     id: ID
@@ -42,13 +37,23 @@ const typeDefs = gql`
   type Games {
     count: Int!
     results: [Results]!
-
+  }
+  type Category {
+    _id: ID
+    name: String
+  }
+  type Product {
+    _id: ID
+    name: String
+    description: String
+    image: String
+    price: Float
+    category: Category
   }
   type playlist {
     _id: ID
-   
+    products: [Product]
   }
-
   type User {
     _id: ID
     firstName: String
@@ -56,23 +61,24 @@ const typeDefs = gql`
     email: String
     playlist: [playlist]
   }
-
   type Auth {
     token: ID
     user: User
   }
-
   type Query {
-   allGames: Games!
-   searchGame(id: Int!): Search!
+    allGames: Games!
+   searchGame(id: String!): GameSearched!
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
     user: User
     playlist(_id: ID!): playlist
   }
-
   type Mutation {
     addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    
+    addplaylist(products: [ID]!): playlist
     updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
     login(email: String!, password: String!): Auth
   }
 `;
