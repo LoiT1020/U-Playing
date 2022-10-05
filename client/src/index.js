e0da300e756fbb17e0ce1e9bc595b9bc2f3b1ff0
Devon Gray <devongray@Devons-MacBook-Pro.local>
2022-10-05 09:14:34 -0500

    import apollo

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
